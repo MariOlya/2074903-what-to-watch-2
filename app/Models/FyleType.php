248b53d4d2c171b15a15077d79db5741cabb71d0
Olga Marinina <youarewonderful@yandex.ru>
2023-03-15 23:10:22 +0700

    Add library for show models

--- app/Models/FyleType.php
+++ app/Models/FyleType.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FileType extends Model
+class FyleType extends Model
 {
     use HasFactory;
 }
