4440d9aa0a1e937768efd5110b5280a9e5395d75
Olga Marinina <youarewonderful@yandex.ru>
2023-03-30 23:03:44 +0700

    Change names of requests

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Http\Requests\Api\FormRequest;
 use Illuminate\Validation\Rules\Password;
 
-class UserLoginRequest extends FormRequest
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
