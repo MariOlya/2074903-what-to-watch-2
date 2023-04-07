99f52e98ce2a344a9f6ac2ec0cdc72a52d74863e
Olga Marinina <youarewonderful@yandex.ru>
2023-04-07 23:43:13 +0700

    Move check unique imdb_id to form request from controller

--- app/Http/Requests/NewFilmRequest.php
+++ app/Http/Requests/NewFilmRequest.php
@@ -6,6 +6,7 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Api\FormRequest;
 use App\Models\Film;
+use Illuminate\Validation\Rule;
 
 class NewFilmRequest extends FormRequest
 {
@@ -19,6 +20,7 @@ class NewFilmRequest extends FormRequest
         return [
             'imdb_id' => [
                 'required',
+                Rule::unique(Film::class),
                 'string',
                 'max:20',
                 'regex:/ev\d{7}\/(19|20)\d{2}(\/[12])?|tt\d{7,8}\/characters\/nm\d{7,8}|(tt|ni|nm)\d{8}|(ch|co|ev|tt|nm)\d{7}/'
