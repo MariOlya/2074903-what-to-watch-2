e73260b981fa56c83de816daeae00bad669d51be
Olga Marinina <youarewonderful@yandex.ru>
2023-04-07 23:31:17 +0700

    Create rules of validation for new film

---
+++ app/Http/Requests/NewFilmRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Api\FormRequest;
+use App\Models\Film;
+
+class NewFilmRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->can('create', Film::class);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'imdb_id' => [
+                'required',
+                'string',
+                'max:20',
+                'regex:/ev\d{7}\/(19|20)\d{2}(\/[12])?|tt\d{7,8}\/characters\/nm\d{7,8}|(tt|ni|nm)\d{8}|(ch|co|ev|tt|nm)\d{7}/'
+            ]
+        ];
+    }
+}
