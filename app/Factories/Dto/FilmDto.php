c51298c9423a25b345c5fbaccf3a555a9c9696cb
Olga Marinina <youarewonderful@yandex.ru>
2023-04-09 19:12:58 +0700

    Change dto for new film

--- app/Factories/Dto/FilmDto.php
+++ app/Factories/Dto/FilmDto.php
@@ -4,11 +4,13 @@ namespace App\Factories\Dto;
 
 class FilmDto extends Dto
 {
+    /**
+     * @param array $params Includes 'name', 'poster_image', 'preview_image', 'background_image', 'background_color',
+     * 'video_link', 'preview_video_link', 'description', 'director', 'starring', 'genre', 'run_time', 'released',
+     * 'imdb_id', 'status'
+     */
     public function __construct(
         array $params,
-        readonly array $fileIds,
-        readonly int $directorId,
-        readonly array $linkIds,
     )
     {
         $this->setParams($params);
