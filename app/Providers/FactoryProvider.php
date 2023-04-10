6f6e1dd439bfa72072efd71ec392c3522bcafbb3
Olga Marinina <youarewonderful@yandex.ru>
2023-04-10 23:07:27 +0700

    Change the way to create some new instances

--- app/Providers/FactoryProvider.php
+++ app/Providers/FactoryProvider.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Factories\ColorFactory;
-use App\Factories\DirectorFactory;
 use App\Factories\FilmFactory;
 use App\Factories\FilmImageFactory;
-use App\Factories\Interfaces\ColorFactoryInterface;
-use App\Factories\Interfaces\DirectorFactoryInterface;
 use App\Factories\Interfaces\FilmFactoryInterface;
 use App\Factories\Interfaces\FilmFileFactoryInterface;
 use App\Factories\Interfaces\LinkFactoryInterface;
@@ -25,9 +21,7 @@ class FactoryProvider extends ServiceProvider
         $this->app->bind(UserFactoryInterface::class, UserFactory::class);
         $this->app->bind(FilmFactoryInterface::class, FilmFactory::class);
         $this->app->bind(FilmFileFactoryInterface::class, FilmImageFactory::class);
-        $this->app->bind(ColorFactoryInterface::class, ColorFactory::class);
         $this->app->bind(LinkFactoryInterface::class, LinkFactory::class);
-        $this->app->bind(DirectorFactoryInterface::class, DirectorFactory::class);
     }
 
 }
