345279f6f6b28a57ddbfd2bccd2ba2f8bfb80161
Olga Marinina <youarewonderful@yandex.ru>
2023-04-14 21:06:25 +0700

    Delete unuseful methods in policies

--- app/Policies/FilmPolicy.php
+++ app/Policies/FilmPolicy.php
@@ -9,22 +9,6 @@ use Illuminate\Auth\Access\Response;
 class FilmPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Film $film): bool
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can create models.
      */
     public function create(User $user): bool
@@ -38,29 +22,5 @@ class FilmPolicy
     public function update(User $user, Film $film): bool
     {
         return $user->userRole->role === User::MODERATOR_ROLE;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Film $film): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Film $film): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Film $film): bool
-    {
-        //
     }
 }
--- app/Policies/ReviewPolicy.php
+++ app/Policies/ReviewPolicy.php
@@ -9,30 +9,6 @@ use Illuminate\Auth\Access\Response;
 class ReviewPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Review $review): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Review $review): bool
@@ -54,21 +30,5 @@ class ReviewPolicy
         }
 
         return $user->id === $review->user_id;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Review $review): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Review $review): bool
-    {
-        //
     }
 }
