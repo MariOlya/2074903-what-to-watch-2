d832175931de73972f71d898718cd40f305e6a90
Olga Marinina <youarewonderful@yandex.ru>
2023-04-22 21:37:04 +0700

    Create the job to parse and fill film info

---
+++ app/Jobs/ParseFilmInfo.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Factories\Dto\FilmApiDto;
+use App\Repositories\Interfaces\FilmApiRepositoryInterface;
+use App\Repositories\Interfaces\FilmRepositoryInterface;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+
+class ParseFilmInfo implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public int $tries = 10;
+    public int $timeout = 120;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        readonly FilmApiRepositoryInterface $apiRepository,
+        readonly string $imdbId,
+        readonly FilmRepositoryInterface $filmRepository,
+    )
+    {
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $response = $this->apiRepository->getMovieInfoById($this->imdbId);
+
+        if ($response['code'] !== 200) {
+            throw new \RuntimeException(
+                message: $response['message'],
+                code: $response['code']
+            );
+        }
+
+        $filmInfo = (array)$response['data'];
+
+        $this->filmRepository->fillFilmInfo($this->imdbId, new FilmApiDto($filmInfo));
+    }
+
+    /**
+     * @param Exception $exception
+     * @return void
+     */
+    public function failed(Exception $exception): void
+    {
+        Log::warning(
+            'Your job to fill film ('.$this->imdbId.') info did not work. There is exception: '.$exception
+        );
+    }
+}
